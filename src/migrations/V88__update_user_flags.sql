7d7c769ec30ce6236663ff2caaab750af2d7407d
idriss.neumann@comwork.io <idriss.neumann@comwork.io>
2024-12-07 14:42:13 +0000

    Release: ce-3.13.13-5b2b4377

---
+++ src/migrations/V88__update_user_flags.sql
@@ -0,0 +1,3 @@
+UPDATE public.user SET enabled_feature = '{"cwaiapi": false, "daasapi": true, "faasapi": true, "auto_pay": false, "billable": false, "emailapi": fal
+se, "monitorapi": true, "k8sapi": true, "without_vat": false, "disable_emails": false, "iotapi": false, "emailapi": true}'
+WHERE email = 'sre-devops@comwork.io';
