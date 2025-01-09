ca820fd5c045252a571aee24b276f470d55f79a3
idriss.neumann@comwork.io <idriss.neumann@comwork.io>
2025-01-09 22:56:31 +0000

    Release: ce-3.15.8-46527fc4

--- src/schemas/Instance.py
+++ src/schemas/Instance.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 from utils.dns_zones import get_first_dns_zone_doc
 from utils.provider import get_provider_infos
 
-DEFAULT_PROVIDER = os.getenv('DEFAULT_PROVIDER', 'void')
+DEFAULT_PROVIDER = os.getenv('DEFAULT_PROVIDER', 'scaleway')
 
 class InstanceUpdateSchema(BaseModel):
     status: Optional[str]
