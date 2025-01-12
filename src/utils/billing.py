4e9c414df7e6a0f07421009a87e2e66a2fdbd7ec
idriss.neumann@comwork.io <idriss.neumann@comwork.io>
2025-01-12 13:37:56 +0000

    Release: ce-3.15.20-8e4d2f5a

--- src/utils/billing.py
+++ src/utils/billing.py
@@ -10,13 +10,13 @@ TIMBRE_FISCAL = get_env_float("TIMBRE_FISCAL", 0.0)
 def download_billing_file(kind, user_id, user_invoice):
     target_name = f"{kind}_{user_invoice.ref}_{user_id}.pdf"
     path_file = "{}/{}".format(datetime.fromisoformat(str(user_invoice.date_created)).strftime("%Y-%m"), target_name)
-    log_msg("INFO", f"[download_billing_file][1] path_file = {path_file}")
+    log_msg("DEBUG", f"[download_billing_file][1] path_file = {path_file}")
     download_status = download_from_invoices_bucket(path_file, target_name)
     if is_false(download_status["status"]):
         target_name = f"{kind}_{user_invoice.ref}.pdf"
         path_file = "{}/{}".format(datetime.fromisoformat(str(user_invoice.date_created)).strftime("%Y-%m"), target_name)
-        log_msg("INFO", f"[download_billing_file][2] path_file = {path_file}")
+        log_msg("DEBUG", f"[download_billing_file][2] path_file = {path_file}")
         download_status = download_from_invoices_bucket(path_file, target_name)
 
-    log_msg("INFO", f"[download_billing_file] download_status = {download_status}")
+    log_msg("DEBUG", f"[download_billing_file] download_status = {download_status}")
     return target_name, download_status
--- src/utils/file.py
+++ src/utils/file.py
@@ -27,12 +27,12 @@ def delete_cert_locally(file_name):
 def get_b64_content(file_name: str, remove: bool):
     b64_content = ""
     with open(file_name, "rb") as file:
-        log_msg("INFO", f"[get_b64_content] writing file {file_name} content in response")
+        log_msg("DEBUG", f"[get_b64_content] writing file {file_name} content in response")
         b64_content = base64.b64encode(file.read()).decode()
         file.close()
 
     if remove:
-        log_msg("INFO", f"[get_b64_content] trying to delete {file_name}")
+        log_msg("DEBUG", f"[get_b64_content] trying to delete {file_name}")
         quiet_remove(file_name)
 
     return b64_content
